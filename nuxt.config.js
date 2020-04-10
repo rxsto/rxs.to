fed5e7898c33d352a9c2ff152ea0fbbe6175cbb2
Oskar <oskar.risto.lang@gmail.com>
2020-04-10 17:29:35 +0200

    Overhaul application and switch to new domain

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,21 +6,21 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, user-scalable=no' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Rxsto\'s public portfolio, including all projects and work. Be sure to check out social media!' },
       { hid: 'google-site-verification', name: 'google-site-verification', content: 'a0LyeGNO7c4b47BfgGaDdIm_NZ_5rWpvhCVUimCm8sQ' },
-      { hid: 'author', name: 'author', content: 'Oskar "Rxsto" Lang (https://rxsto.me)' },
-      { hid: 'publisher', name: 'publisher', content: 'Oskar "Rxsto" Lang (https://rxsto.me)' },
-      { hid: 'image', name: 'image', content: 'https://rxsto.me/img/avatar.png' },
+      { hid: 'author', name: 'author', content: 'Oskar "Rxsto" Lang (https://rxs.to)' },
+      { hid: 'publisher', name: 'publisher', content: 'Oskar "Rxsto" Lang (https://rxs.to)' },
+      { hid: 'image', name: 'image', content: 'https://rxs.to/img/rxsto_rounded.png' },
       { hid: 'robots', name: 'robots', content: 'index, follow' },
       { hid: 'application-name', name: 'application-name', content: 'Rxsto' },
       { hid: 'theme-color', name: 'theme-color', content: '#cedb9d' },
       { hid: 'og:title', property: 'og:title', content: 'Rxsto' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'Rxsto' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: 'https://groovy.bot' },
+      { hid: 'og:url', property: 'og:url', content: 'https://rxs.to' },
       { hid: 'og:description', property: 'og:description', content: '' },
       { hid: 'og:locale', property: 'og:locale', content: 'en_US' },
-      { hid: 'og:image', property: 'og:image', content: 'https://rxsto.me/img/avatar.png' },
+      { hid: 'og:image', property: 'og:image', content: 'https://rxs.to/img/rxsto_rounded.png' },
       { hid: 'og:image:type', property: 'og:image:type', content: 'image/png' },
       { hid: 'og:image:width', property: 'og:image:width', content: '750' },
       { hid: 'og:image:height', property: 'og:image:height', content: '750' },
@@ -29,8 +29,8 @@ export default {
       { hid: 'twitter:creator', name: 'twitter:creator', content: '@rxsto_official' }
     ],
     link: [
-      { rel: 'canonical', href: 'https://rxsto.me' },
-      { rel: 'icon', type: 'image/x-icon', href: '/img/rxsto_rounded.png' },
+      { rel: 'canonical', href: 'https://rxs.to' },
+      { rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico' },
       { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Space+Mono:400,700' },
       { rel: 'stylesheet', type: 'text/css', href: 'https://use.fontawesome.com/releases/v5.7.1/css/all.css' }
     ]
@@ -45,7 +45,6 @@ export default {
   ],
 
   modules: [
-    '@nuxtjs/axios',
     '@nuxtjs/pwa'
   ],
 
