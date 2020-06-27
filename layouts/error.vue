2d9f3b665e4cef6f7c8853d8f2560a8993fc2769
Oskar Lang <oskar.risto.lang@gmail.com>
2020-06-27 22:13:42 +0200

    feat: add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <p class="text-6xl font-bold">
+      {{ code }}
+    </p>
+    <h1 class="text-3xl mt-4">
+      Oh no! 😔
+    </h1>
+    <p class="text-lg text-center mx-4">
+      {{ message }}
+    </p>
+    <nuxt-link class="flex items-center mt-4 text-green-base" to="/">
+      <fa icon="arrow-left" />
+      <p class="ml-4">
+        back to home
+      </p>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    code () {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message () {
+      return this.error.message || '<%= messages.client_error %>'
+    }
+  },
+  head () {
+    return {
+      title: `${this.code} - ${this.message.substring(0, 55)}`
+    }
+  }
+}
+</script>
