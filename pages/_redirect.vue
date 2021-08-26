7fd8f0826e8f5af2c66f967c667a51e1eba5126b
Oskar Lang <oskar.risto.lang@gmail.com>
2021-08-26 15:39:34 +0200

    fix: stay in tab while redirecting

--- pages/_redirect.vue
+++ pages/_redirect.vue
@@ -9,8 +9,7 @@ export default {
   mounted () {
     const redirect = redirects.find(redirect => redirect.id === this.$route.params.redirect)
     window.open(
-      redirect.url,
-      '_blank'
+      redirect.url
     )
   }
 }
