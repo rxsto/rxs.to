d580da34f76ccd8e5235305188ce2e9ed2c34692
Oskar Lang <oskar.risto.lang@gmail.com>
2021-08-31 10:39:52 +0200

    fix: replace current location on redirect instead of opening up another popup

--- pages/_redirect.vue
+++ pages/_redirect.vue
@@ -8,9 +8,7 @@ import redirects from '~/assets/json/core/redirects'
 export default {
   mounted () {
     const redirect = redirects.find(redirect => redirect.id === this.$route.params.redirect)
-    window.open(
-      redirect.url
-    )
+    location.replace(redirect.url)
   }
 }
 </script>
