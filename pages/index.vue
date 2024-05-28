dcaa7e3d960bb186d8f7a4e18140e74999535899
Oskar Lang <oskar.risto.lang@gmail.com>
2024-05-28 10:38:58 +0200

    feat: reword introduction sentence

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <h1 class="font-bold uppercase flex flex-col items-center text-foreground text-opacity-75">
         <p class="text-3xl sm:text-5xl md:text-6xl lg:text-7xl flex items-center gap-3 md:gap-5">Hey there, I'm <img src="~/assets/img/rxsto.svg"
             alt="rxsto" class="h-[20px] sm:h-[32px] md:h-[42px] lg:h-[49px] glow" /></p>
-        <p class="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center px-4">but you might also know me as oz or Oskar.</p>
+        <p class="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center px-4">but you can also call me oz or Oskar.</p>
       </h1>
       <h2 class="text-base md:text-lg lg:text-xl opacity-90 font-medium text-center px-4">
         I'm a self-employed freelance full stack bizdevops engineer.
