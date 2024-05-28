41d1c98559e5e3fdb3f9d0c71e20335f826a44e2
Oskar Lang <oskar.risto.lang@gmail.com>
2024-05-28 10:22:37 +0200

    feat: finalize navigation splash

--- components/core/navigation/CoreNavigationSplash.vue
+++ components/core/navigation/CoreNavigationSplash.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="h-screen w-screen fixed bg-background bg-opacity-90 backdrop-blur-sm z-30 top-0 left-0 overscroll-none">
-    <!-- <button @click="close" class="absolute top-8 right-8 h-12 interactable">close</button> -->
     <div class="flex flex-col h-full justify-evenly items-center py-16 md:p-16">
-      <NuxtLink v-for="el in nav" :to="el.url" @click="close"
-        class="text-3xl md:text-5xl font-bold uppercase text-center tracking-[1.5rem] indent-[2rem] md:tracking-[3rem] md:indent-[2rem] z-20 opacity-80 hover:opacity-100 transition select-none interactable">
-        {{ el.display }}
+      <NuxtLink v-for="(el, i) in nav" :to="el.url" @click="close"
+        class=" h-[48px] overflow-hidden text-3xl md:text-5xl font-bold uppercase text-center tracking-[1.5rem] indent-[2rem] md:tracking-[3rem] md:indent-[2rem] z-20 opacity-80 hover:opacity-100 transition select-none interactable">
+        <p class="animated" :style="`animation-delay: ${i * 75}ms;`">
+          {{ el.display }}
+        </p>
       </NuxtLink>
     </div>
   </div>
@@ -21,3 +22,23 @@ const close = () => {
 
 onKeyStroke('Escape', () => close())
 </script>
+
+<style scoped>
+@keyframes animated {
+  from {
+    transform: translateY(48px);
+  }
+
+  to {
+    transform: translateY(0);
+  }
+}
+
+.animated {
+  transform: translateY(-48px);
+  animation-name: animated;
+  animation-duration: 300ms;
+  animation-timing-function: cubic-bezier(0.68, -0.6, 0.32, 1.6);
+  animation-fill-mode: forwards;
+}
+</style>
